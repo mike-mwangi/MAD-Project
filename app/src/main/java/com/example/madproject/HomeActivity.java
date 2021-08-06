aa81247454ba3a94f7aac41a5cf6841a9ebb378e
Mike Mwangi <mikemwangi0551@gmail.com>
2021-08-06 20:41:38 +0300

    Bottom navigation done

---
+++ app/src/main/java/com/example/madproject/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
